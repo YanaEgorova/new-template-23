e1b56443292ae655da96222169dd7428c72acf80
Yana <yanaegorova69.3@gmail.com>
2021-09-14 22:04:02 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,156 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Handheld Milk Frother Mixer',
+        price: 6.96,
+        image: './img/img1.png',
+        description: [
+            {
+                text: 'Make perfect froth toppings.',
+            },
+            {
+                text: 'Easy and fun to use.',
+            },
+            {
+                text: 'Battery operated.',
+            },
+            {
+                text: 'Portable and lightweight.',
+            },
+            {
+                text: 'Hand wash and dry completely.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png', './img/img1-3.png', './img/img1-4.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Cooling Elevated Pet Bed',
+        price: 29.99,
+        discount: 24.65,
+        image: './img/img2.png',
+        description: [
+            {
+                text: 'Breathable mesh fabric helps keep pets cool by allowing air to flow.',
+            },
+            {
+                text: 'Elevates pets more than 7 inches off the ground.',
+            },
+            {
+                text: 'Easy to clean (using tap water only); simple assembly (screws and hex tool included).',
+            },
+            {
+                text: 'Recommended for large sized breeds; like a German Shepherd or Boxer.',
+            },
+            {
+                text: 'Size L; Overall Dimensions: 51.3 x 31.5 x 7.7 inches; Mesh Dimensions: 44.1 x 32.3 inches.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png', './img/img2-3.png'],
+    },
+    {
+        id: 'product-3',
+        name: '2-Way Manifold Gauge Set',
+        price: 64.90,
+        discount: 69.99,
+        image: './img/img3.png',
+        description: [
+            {
+                text: '100% U.S. designed, assembled and tested.',
+            },
+            {
+                text: 'HEAVY DUTY FORGED BRASS BODY gives life-long durability and endurance.',
+            },
+            {
+                text: 'FREE FLOATING PISTON TYPE VALVES allow for less friction and internal wear and tear extending the life of the internal o-rings.',
+            },
+            {
+                text: 'GRADE 5 LEVEL HIGH PRESSURE HOSES with ¼” SAE standard fittings using the highest technology rubber materials giving the hoses the most flexibility and reducing the risk of kinking.',
+            },
+            {
+                text: '3 1/8” SILICONE DAMPENED GAUGES smooth out needle movement while the color coding of R410a, R22 and R404A provides immediate refrigerant recognition.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.png', './img/img3-4.png', './img/img3-5.png', './img/img3-6.png', './img/img3-7.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Cordless Tire Inflator',
+        price: 98.90,
+        discount: 55.97,
+        image: './img/img4.png',
+        description: [
+            {
+                text: 'Air compressor tire inflator comes with high-pressure inflation mode with automatic shutoff.',
+            },
+            {
+                text: 'Digital tire inflator has high-volume inflation and deflation modes.',
+            },
+            {
+                text: 'LED light of the portable tire inflator for car helps to provide illumination.',
+            },
+            {
+                text: 'Three power sources for inflation on a jobsite and on the go (e.g. 20V MAX* battery, 12V DC, or 110V AC).',
+            },
+            {
+                text: 'Convenient onboard accessory storage.',
+            },
+            {
+                text: 'Heavy-duty rubber feet help reduce walking when in use.',
+            },
+            {
+                text: 'Threaded chuck for secure connection to inflation valve.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png', './img/img4-3.png', './img/img4-4.png', './img/img4-5.png', './img/img4-6.png', './img/img4-7.jpg', './img/img4-8.png', './img/img4-9.png', './img/img4-10.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Portable Folding Camp Kitchen',
+        price: 119.98,
+        discount: 79.93,
+        image: './img/img5.png',
+        description: [
+            {
+                title: 'Folding Camping Table',
+                text: 'Folding camp table brings the kitchen outside with enough counter and storage space to accommodate camp kitchen equipment like stoves and grills, cooking utensils, and cookware.',
+            },
+            {
+                title: 'Quality Construction',
+                text: '1-piece powder-coated steel outdoor table frame easily unfolds to reveal an aluminum countertop, storage rack, 3 fold-out side tables, collapsible sink, and lantern pole.',
+            },
+            {
+                title: 'Specialized Shelving',
+                text: 'Outdoor prep table features heat-resistant countertop (48-lb. weight limit) for camp stoves, a lower rack (35-lb. weight capacity) for dry good storage, and a collapsible softshell sink with drain for dishes.',
+            },
+            {
+                title: 'Spacious Storage',
+                text: 'Foldable table features 3 fold-out side tables with space for food and beverages, a paper towel holder, and hooks for grilling utensils and garbage bags.',
+            },
+            {
+                title: 'Compact',
+                text: 'Portable table with carry handles folds flat for storage; Measures 21.7 x 56.3 x 32.3 inches open, 5.9 x 21.7 x 34.8 inches folded, and weighs 22.7 pounds.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png', './img/img5-3.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Door Wall GearBox',
+        price: 148.00,
+        discount: 95.50,
+        image: './img/img6.png',
+        description: [
+            {
+                text: 'Easy to assemble - the tab and loop design on all the panels minimizes the need for hardware and removes guesswork in assembly.',
+            },
+            {
+                text: 'Powder coat finish electrostatically bonds and bakes the protective coating to the bare metal, forming an effective barrier against corrosion.',
+            },
+            {
+                text: 'Package Weight: 14.882 kilograms.',
+            },
+        ],
+        images: ['./img/img6-1.png', './img/img6-2.png', './img/img6-3.png', './img/img6-4.png'],
+    },
+]
